6a9f0c5b0436aa51aad93720dda00d95fb38a488
Yoav Abrahami <yoav@wix.com>
2012-12-06 11:05:45 +0200

    improved thread stopping tests

--- src/test/java/com/wixpress/tutorial/concurrency/ThreadStoppingTest.java
+++ src/test/java/com/wixpress/tutorial/concurrency/ThreadStoppingTest.java
@@ -6,6 +6,13 @@ import org.junit.Rule;
 import org.junit.Test;
 import sun.awt.windows.ThemeReader;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
@@ -49,16 +56,16 @@ public class ThreadStoppingTest {
     }
 
     @Test
-    public void terminateIOThread() throws InterruptedException {
+    public void terminateSleepingThread() throws InterruptedException {
         Runnable runnable = new Runnable() {
             @Override
             public void run() {
                 try {
-                testLogger.log().debug("thread - started");
-                for (long i=0; i < 100l; i++)
-                    Thread.sleep(1);
-                count += 1;
-                testLogger.log().debug("thread - completed");
+                    testLogger.log().debug("thread - started");
+                    for (long i=0; i < 100l; i++)
+                        Thread.sleep(1);
+                    count += 1;
+                    testLogger.log().debug("thread - completed");
                 } catch (InterruptedException e) {
                     testLogger.log().debug("thread - interrupted");
                 }
@@ -80,6 +87,55 @@ public class ThreadStoppingTest {
     }
 
     @DoesNotWork
+    @Test
+    public void terminateIOThread() throws InterruptedException {
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    testLogger.log().debug("thread - started");
+                    for (long i=0; i < 100l; i++) {
+                        URL url = new URL("http://www.google.com");
+                        HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+                        BufferedReader bf = null;
+                        try {
+                            connection.getResponseCode();
+                            bf = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                            String line;
+                            while (((line = bf.readLine()) != null)) {
+                                ;
+                            }
+                        }
+                        finally {
+                            if (bf != null)
+                                bf.close();
+                            connection.disconnect();
+                        }
+                    }
+                    count += 1;
+                    testLogger.log().debug("thread - completed");
+                } catch (MalformedURLException e) {
+                    testLogger.log().debug("thread - url exception");
+                } catch (IOException e) {
+                    testLogger.log().debug("thread - io exception {}", e.getClass());
+                }
+            }
+        };
+
+        Thread thread = new Thread(runnable);
+
+        testLogger.log().debug("starting the thread");
+        thread.start();
+        Thread.sleep(10);
+        testLogger.log().debug("interrupt");
+        thread.interrupt();
+        testLogger.log().debug("waiting for the thread to complete");
+        thread.join();
+        testLogger.log().debug("thread completed and joined");
+        assertThat(count, is(0));
+
+    }
+
     @Test
     public void stopCPUThread() throws InterruptedException {
         Runnable runnable = new Runnable() {
@@ -107,35 +163,5 @@ public class ThreadStoppingTest {
 
     }
 
-    @Test
-    public void stopIOThread() throws InterruptedException {
-        Runnable runnable = new Runnable() {
-            @Override
-            public void run() {
-                try {
-                testLogger.log().debug("thread - started");
-                for (long i=0; i < 100l; i++)
-                    Thread.sleep(1);
-                count += 1;
-                testLogger.log().debug("thread - completed");
-                } catch (InterruptedException e) {
-                    testLogger.log().debug("thread - interrupted");
-                }
-            }
-        };
-
-        Thread thread = new Thread(runnable);
-
-        testLogger.log().debug("starting the thread");
-        thread.start();
-        Thread.sleep(10);
-        testLogger.log().debug("interrupt");
-        thread.stop();
-        testLogger.log().debug("waiting for the thread to complete");
-        thread.join();
-        testLogger.log().debug("thread completed and joined");
-        assertThat(count, is(0));
-
-    }
 
 }
